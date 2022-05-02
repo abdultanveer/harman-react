46432005d8de9ba8966695cc4d56dab91366f00f
abdul <abdul.tanveer@gmail.com>
2022-05-02 11:18:34 +0530

    change state

--- src/App.js
+++ src/App.js
@@ -2,18 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
+import Login, { Message } from './components/Message'
 
 function App() {
   return (
     <div className="App">
     
-      <Greet name ="abdul" alias ="tan">
-          <p>this is children</p>
-      </Greet>
-      <Greet name ="ansari" alias ="an">
-        <button>click me</button>
-      </Greet>
-      <Greet name ="harman" alias ="hmm"/>
+      <Message/>
 
      {/* <Welcome name="abdul" alias =" ansari"/> */}
     </div>
