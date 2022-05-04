68ff3663c1778b90c30106d3476a7781d02509bb
abdul <abdul.tanveer@gmail.com>
2022-05-04 10:47:34 +0530

    unique key for each person

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -4,14 +4,14 @@ import Person from './Person'
 function NameList() {
   const persons = [
     { id: 1, name: 'John', age: 36, skill: 'React' },
-    { id: 1, name: 'abdul', age: 12, skill: 'java' },
-    { id: 1, name: 'aditya', age: 34, skill: 'python' },
-    { id: 1, name: 'hritik', age: 56, skill: 'javaxript' }
+    { id: 2, name: 'abdul', age: 12, skill: 'java' },
+    { id: 3, name: 'aditya', age: 34, skill: 'python' },
+    { id: 4, name: 'hritik', age: 56, skill: 'javaxript' }
   ]
 
   const personList = persons.map(
     person => (
-      <Person personKey={person} />
+      <Person key = {person.id} personKey={person} />
     )
   )
 
