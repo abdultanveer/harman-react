5a7c8e4fc18885e1b36d90d3b28b39a0f19d8f3a
abdul <abdul.tanveer@gmail.com>
2022-05-04 10:37:34 +0530

    json data to a list

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,15 +1,26 @@
 import React from 'react'
 
 function NameList() {
-const names = ['bruce', 'clark','abdul','ansari']
+const persons = [
+{  id: 1, name: 'John', age: 36, skill: 'React' },
+{  id: 1, name: 'abdul', age: 12, skill: 'java' },
+{  id: 1, name: 'aditya', age: 34, skill: 'python' },
+{  id: 1, name: 'hritik', age: 56, skill: 'javaxript' }
+]
+
+const personList = persons.map(
+  person => (
+    <h2>
+        I am {person.name}, my age is {person.age} i know {person.skill}
+    </h2>
+  )
+)
+
+
 
   return (
     <div>
-    {
-      names.map(name => <h2>{name}</h2>)
-    }
-
-
+      { personList}
     </div>
   )
 }
