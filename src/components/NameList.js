7dc0c068b4c6b8a595055651791739545bca7279
abdul <abdul.tanveer@gmail.com>
2022-05-04 09:51:27 +0530

    display array in a list  using map function

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -5,10 +5,10 @@ const names = ['bruce', 'clark','abdul','ansari']
 
   return (
     <div>
-        <h2>{ names[0] }</h2>
-        <h2>{ names[1] }</h2>
-        <h2>{ names[2] }</h2>
-        <h2>{ names[3] }</h2>
+    {
+      names.map(name => <h2>{name}</h2>)
+    }
+
 
     </div>
   )
